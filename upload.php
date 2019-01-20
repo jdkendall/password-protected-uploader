e2541d870da43e71e9c7fb4b5413864531f50b39
jdkendall <justin.d.kendall@gmail.com>
2019-01-20 15:52:45 -0600

    Adjust & refactor filetype restriction rules

--- upload.php
+++ upload.php
@@ -1,18 +1,14 @@
 <?php
+$MAX_FILE_SIZE_IN_MB = 200;
+$CHECK_FILE_TYPES = true;
+$FILE_TYPES_ALLOWED = ["pdf", "jpg", "jpeg", "png", "gif", "bmp"];
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
+
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+    $uploadOk = 1;
 }
 // Check if file already exists
 if (file_exists($target_file)) {
@@ -20,14 +16,13 @@ if (file_exists($target_file)) {
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
+if ($_FILES["fileToUpload"]["size"] > $MAX_FILE_SIZE_IN_MB * 1024 * 1024) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+if($CHECK_FILE_TYPES && !in_array(strtolower($imageFileType), $FILE_TYPES_ALLOWED)) {
+    echo "Sorry, this file type is not allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
